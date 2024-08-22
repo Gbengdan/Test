3c5928546e748f41c1d2c9b809f23342ed59d3de
Gbengdan <gbengdan@gmail.com>
2024-08-22 20:48:50 +0100

    first push

---
+++ app.js
@@ -0,0 +1,27 @@
+// alert("Hello world")
+
+const name ="Tester"
+
+// alert(name)
+var school ="lagos state university"
+var names ="gbenga daniel" 
+
+console.log(names)
+
+console.log(name)
+
+console.log(school)
+
+let course = "javascript"
+let age = "54"
+
+console.log(age)
+
+const topic = "variables is our topic and we are looking for it"
+
+const year ="2024"
+
+console.log(topic)
+console.log(year)
+
+console.log(course)
